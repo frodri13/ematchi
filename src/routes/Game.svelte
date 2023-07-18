227f2c3bbdfe2f73efd174244753664e20d6d879
Fabio Rodriguez <alessf13@gmail.com>
2023-07-18 12:13:34 +0100

    gird component

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Grid from "./Grid.svelte";
+
+</script>
 <div class="flex flex-col items-center justify-center h-full">
    
     <div class="w-[80vmin] h-[10vmin] bg-purple-600">
@@ -5,7 +9,7 @@
     </div>
 
     <div class="w-[80vmin] h-[80vmin] bg-teal-400">
-
+        <Grid />
     </div>
 
     <div class="w-[80vmin] h-[10vmin] bg-purple-600">
---
+++ src/routes/Grid.svelte
@@ -0,0 +1,3 @@
+<div class="grid grid-cols-4 grid-rows-4">
+
+</div>
