a48988dda053c55d76c4f47cb0dff4da6a3f700d
Fabio Rodriguez <alessf13@gmail.com>
2023-07-19 10:00:13 +0100

    logic for selecting pairs

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -3,10 +3,37 @@
 
   export let grid: string[];
 
+  let a = -1;
+  let b = -1;
+  let reset_timeout: ReturnType<typeof setTimeout>;
 </script>
 <div class="grid grid-cols-4 grid-rows-4 h-full gap-[0.5em]">
-    {#each grid as emoji}
-        <Square {emoji} />
+    {#each grid as emoji, i}
+        <Square 
+          {emoji}
+          selected={a === i || b === i} 
+
+          on:click={() => {
+            clearTimeout(reset_timeout)
+              if(a === -1 && b === -1){
+                 a = i;
+              } else if (b === -1) {
+                 b = i;
+
+                if (grid[a] === grid[b]) {
+                  // correct
+                } else {
+                  // incorrect
+                  reset_timeout = setTimeout(() => {
+                    a = b = -1;
+                  }, 1000);
+                }
+              } else {
+                b = -1;
+                a = i;
+              }
+            }
+          }/>
     {/each}
 
 </div>
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
     export let emoji: string;
+    export let selected: boolean;
 </script>
-<div class="bg-yellow-200">
-    <span>{emoji}</span>
+<div class="bg-yellow-200 text-7xl flex justify-center items-center" class:flipped={selected}>
+    <button on:click class="absolute w-full h-full"/>
+    <span class="pointer-events-none">{emoji}</span>
 </div>  
+
+<style>
+    .flipped button {
+        background: red;
+    }
+</style>
